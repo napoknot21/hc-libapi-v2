d4477a716286fadd365f628ea771e8a643c8f3dc
napoknot21 <napoknot21@protonmail.com>
2025-08-07 18:19:37 +0200

    Changes in parameters and new relaod function

--- src/libApi/config/parameters.py
+++ src/libApi/config/parameters.py
@@ -1,13 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# GLOBAL INFORMATION VARIABLES ICE API
+from libApi.config.env import load_dotenv
 
 ICE_HOST=os.getenv("ICE_HOST")
 ICE_AUTH=os.getenv("ICE_AUTH")
-
 
 ICE_USERNAME=os.getenv("ICE_USERNAME")
 ICE_PASSWORD=os.getenv("ICE_PASSWORD")
@@ -32,26 +27,37 @@ FX_PRICER_SOLVE_PATH=os.getenv("FX_PRICER_SOLVE_PATH")
 EQ_PRICER_CALC_PATH=os.getenv("EQ_PRICER_CALC_PATH")
 EQ_PRICER_SOLVE_PATH=os.getenv("EQ_PRICER_SOLVE_PATH")
 
+
 API_LOG_REQUEST_FILE_PATH=os.getenv("API_LOG_REQUEST_FILE_PATH")
 FILE_ID_CALCULATION_PATH=os.getenv("FILE_ID_CALCULATION_PATH")
+FILE_ID_CALCULATION_CSV_PATH=os.getenv("FILE_ID_CALCULATION_CSV_PATH")
+
 PRICING_LOG_FILE_PATH=os.getenv("PRICING_LOG_FILE_PATH")
+
 SAVED_REQUESTS_DIRECTORY_PATH=os.getenv("SAVED_REQUESTS_DIRECTORY_PATH")
+
+
+BOOK_NAMES_HV_ALL=os.getenv("BOOK_NAMES_HV_ALL")
+BOOK_NAMES_WR_ALL=os.getenv("BOOK_NAMES_WR_ALL")
+
+BOOK_NAMES_HV_SUBSET_N1=os.getenv("BOOK_NAMES_HV_SUBSET_N1")
+BOOK_NAMES_HV_SUBSET_N2=os.getenv("BOOK_NAMES_HV_SUBSET_N2")
+
+
+# Optional parameters (to avoid to convert every time the list)
+
+BOOK_NAMES_HV_LIST_ALL=list(BOOK_NAMES_HV_ALL.split(","))
+BOOK_NAMES_WR_LIST_ALL=list(BOOK_NAMES_WR_ALL.split(","))
+
+BOOK_NAMES_HV_LIST_SUBSET_N1=list(BOOK_NAMES_HV_SUBSET_N1.split(","))
+BOOK_NAMES_HV_LIST_SUBSET_N2=list(BOOK_NAMES_HV_SUBSET_N2.split(","))
 
 
 # instruments concurrencies
 CCYS_ORDER=['EUR', 'USD', 'CHF', 'CAD', 'JPY', 'GBP', 'SEK', 'NOK']
 
 
-# Book names
-BOOK_NAMES=(os.getenv("BOOK_NAMES")).split(",")
-BOOK_N_SUBBOOK_NAMES=(os.getenv("BOOK_N_SUBBOOK_NAMES")).split(",")
-
-BOOK_NAME_HV=(os.getenv("BOOK_NAMES_HV")).split(",")
-BOOK_NAME_WR=(os.getenv("BOOK_NAMES_WR")).split(",")
-
-
-# Counterparties and their names
-counterparties = [
+COUNTERPARTIES=[
 
     {
         "name" : os.getenv("NAME_COUNTER_GSI"),
@@ -61,7 +67,7 @@ counterparties = [
         "nameInIce" : os.getenv("NAME_ICE_GSI")
     },
 
-        {
+    {
         "name" : os.getenv("NAME_COUNTER_MS"),
         "email" : os.getenv("EMAIL_COUNTER_MS"),
         "mailSubject" : os.getenv("MAIL_SUBJECT_2"),
@@ -73,7 +79,7 @@ counterparties = [
 
 
 # Columns in the pricer
-columnsInPricer = {
+COLUMNS_IN_PRICER={
 
     "price" : "sum",
     "priceCurrency" : "first",
@@ -144,14 +150,14 @@ columnsInPricer = {
     "VegaTerm_currency":"first",
     "VolatilitySpread":"first",
     "direction":" ".join,
-    "pair":"first",
-    "opt_type":"first",
-    "strike":"first",
-    "notional":"first",
-    "notional_currency":"first",
-    "expiry":"first",
-    "strike":" ".join,
-    "CurrentNotional":" ".join,
+    "pair" : "first",
+    "opt_type" : "first",
+    "strike" : "first",
+    "notional" : "first",
+    "notional_currency" : "first",
+    "expiry" : "first",
+    "strike" : " ".join,
+    "CurrentNotional": " ".join,
     "CurrentNotional_currency":"first",
     "MarketValueAsk":"sum",
     "MarketValueAsk_currency":"first",
